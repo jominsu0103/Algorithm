c074c49f6271eb0cd4cd929330ad5311f77ea80a
usnim <134982212+jominsu0103@users.noreply.github.com>
2024-02-01 18:15:35 +0900

    [level 0] Title: 문자열 돌리기, Time: 91.62 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    const rotate = (str) => {
+        const line = str.split('');
+        
+        for(let i = 0 ; i < line.length; i++){
+            console.log(line[i]);
+        }
+    }
+    rotate(str);
+});
